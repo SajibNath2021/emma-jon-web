fc65b7e6e614224275798822ab24b897b5f50ca8
Sajib Nath <sajibnath1996@gmail.com>
2021-02-26 14:56:55 +0600

    first page done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total , prd)=> total + prd.price, 0);
+    //or
+    // let total =0;
+    // for(let i =  0; i < cart.length;i++){
+    //     const prd = cart[i];
+    //     total = total + prd.price; 
+    // }
+    let Shipping = 0;
+    if(total > 35){
+        Shipping = 0;
+    } 
+    else if (total > 15){
+        Shipping = 4.99;
+    }
+    else if (total > 0){
+        Shipping = 12.99
+    }
+
+    const tex = (total /10);
+
+    const formatNumber = num =>{
+        const number = num.toFixed(2);
+        return Number(number);
+    } 
+
+    return (
+        <div>
+            <h3>Order Summary</h3>
+            <p>Items Ordered : {cart.length}</p>
+            <p>Product Price : {formatNumber(total)}</p>
+            <p>Shipping Cost : {Shipping}</p>
+            <p>Tex + Vat :{formatNumber(tex)}</p>
+            <p>Total Price : ${formatNumber(total + Shipping + tex)}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
@@ -26,8 +27,7 @@ const Shop = () => {
                   
                 </div>
                 <div className="cart-container">
-                    <h3>this is cart</h3>
-                    <h4>total summary : { cart.length}</h4>
+                    <Cart cart = {cart}></Cart>
                 </div>
             </div>
             
